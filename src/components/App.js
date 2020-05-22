06b4259c74d5cce790413949fd9882a0f53d9049
achref essid <essid.developpeur@gmail.com>
2020-05-22 18:02:17 +0100

    Setup Redux & Bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class App extends Component {
     render() {
         return (
-            <h2>Reminders App</h2>
+            <h2 className="btn btn-danger">Reminder App</h2>
         )
     }
 }
 
 export default App
+
+
+
